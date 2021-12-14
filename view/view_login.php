abd554a1fc00a56b92efde67520b352c6a955a51
blade <blade@kali>
2021-12-14 10:44:59 +0700

    update view, add recaptcha api js

--- view/view_login.php
+++ view/view_login.php
@@ -37,5 +37,13 @@
 
     <?= form_close(); ?>
 </div>
+    
+    
+<!-- jQuery 3 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!--reCAPTCHA-->
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </body>
 </html>
